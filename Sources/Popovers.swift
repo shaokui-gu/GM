c233d3fcef0a3bc80292ed47ccd5af1af8de7b34
gsk <gsk@kroknow.com>
2021-12-23 19:33:57 +0800

    fix popover color

--- Sources/Popovers.swift
+++ Sources/Popovers.swift
@@ -313,6 +313,10 @@ open class GMPopoverSwiftUIView<Content>: UIHostingController<Content> where Con
     public override init(rootView: Content) {
         super.init(rootView: rootView)
     }
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .clear
+    }
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
