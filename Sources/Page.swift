d83dea3065a5d0d4dfba77f3f4cf8db4485de5f5
gsk <gsk@kroknow.com>
2022-02-11 21:34:02 +0800

    add GMGridPage

--- Sources/Page.swift
+++ Sources/Page.swift
@@ -231,6 +231,55 @@ open class GMListPage : UITableViewController, GMPageLifeCycle {
     open func onPageBoundsUpdated(_ bounds: CGRect) {}
 }
 
+open class GMGridPage : UICollectionViewController, GMPageLifeCycle {
+    
+    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.onPageInit()
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    final public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.onPageLoaded()
+    }
+    
+    final public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.onPageAppear()
+    }
+
+    final public override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    final public override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    final public override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.onPageDisappear()
+    }
+    
+    deinit {
+        if let identifire = self.routeIdentifire {
+            Router.shared.removePage(identifire)
+        }
+        self.onPageDestroy()
+    }
+    
+    open func onPageAppear() -> Void {}
+    open func onPageDisappear()  -> Void {}
+    open func onPageLoaded() -> Void {}
+    open func onPageInit() -> Void {}
+    open func onPageDestroy() -> Void {}
+    open func onPageBoundsUpdated(_ bounds: CGRect) {}
+}
+
 /// Swift UI
 @available(iOS 13.0, *)
 public protocol GMSwiftUIPageView : View {
